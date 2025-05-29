fde8a79982ada643f88ca8d2aae8eb33ef7d99c5
namezor90 <namezor90@gmail.com>
2025-05-29 19:07:02 +0200

    Delete src directory

--- src/controllers/HomeController.php
+++
@@ -1,7 +0,0 @@
-<?php
-// src/controllers/HomeController.php
-class HomeController {
-    public function index() {
-        include 'src/views/home/index.php';
-    }
-}
--- src/controllers/MapController.php
+++
@@ -1,7 +0,0 @@
-<?php
-// src/controllers/MapController.php
-class MapController {
-    public function index() {
-        include 'src/views/map/index.php';
-    }
-}
